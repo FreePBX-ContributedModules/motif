d9064e18cdb8d42f59c6ac2d4541109c0ca23ff3
Rob Thomas <xrobau@gmail.com>
2014-01-08 06:11:11 +1000

    Update to use Sipsettings RTP config, rather than core.

--- functions.inc.php
+++ functions.inc.php
@@ -3,16 +3,9 @@
 global $amp_conf;
 
 function motif_get_config($engine) {
-	global $db;
-	global $amp_conf;
-	global $ext;  // is this the best way to pass this?
-	global $asterisk_conf;
-	global $core_conf;
-	global $version;
-
 	switch($engine) {
 		case "asterisk":
-			$core_conf->addRtpAdditional('general', array("icesupport" => "yes"));
+			FreePBX::create()->Sipsettings->setConfig('icesupport', 'yes');
 			break;
 	}
 }
